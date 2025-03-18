783e8917a0442e5f2f87dc8b1367ef8434a39d03
RafaelTeixeiraMiguel <teixeirarafael12@gmail.com>
2025-03-18 00:04:28 -0300

    Adicionado validações para o campo Code

--- Ucondo.Evaluation.Domain/Validation/CodeValidator.cs
+++ Ucondo.Evaluation.Domain/Validation/CodeValidator.cs
@@ -15,7 +15,21 @@ namespace Ucondo.Evaluation.Domain.Validation
             .NotNull()
             .WithMessage("The code must not be null.")
             .NotEmpty()
-            .WithMessage("The code must not be empty.");
+            .WithMessage("The code must not be empty.")
+            .Matches(@"^(\d+)(\.\d+)*$")
+            .WithMessage("The code must follow the correct pattern (n.n.n...).")
+            .Must(BeValidSequencialNumber)
+            .WithMessage("The code sequencial number must be between 1 and 999.");
+        }
+
+        private bool BeValidSequencialNumber(string code)
+        {
+            var codeParts = code.Split('.');
+            var lastSegmentStr = codeParts.Last();
+
+            int.TryParse(lastSegmentStr, out int lastSegment);
+
+            return !(lastSegment > 999 || lastSegment < 1);
         }
     }
 }
